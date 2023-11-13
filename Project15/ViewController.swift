118d659b13cf3d4d26f6ba8cbb5c529413452e01
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-11-13 21:41:12 +0100

    Added imageView to the view controller

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private var imageView: UIImageView!
+    private var currentAnimation = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        
+        view.addSubview(imageView)
     }
-
-
+    
+    @IBAction private func buttonTapped(_ sender: Any) {
+        currentAnimation += 1
+        
+        if currentAnimation > 7 { currentAnimation = 0 }
+    }
 }
-
